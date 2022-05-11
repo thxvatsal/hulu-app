1ff7eb461a5fb7f3ada88bea575ef281d6c068b4
vat.sal <0505m2003@gmail.com>
2022-05-11 10:47:16 +0530

    Adding Links

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -12,17 +12,19 @@ import {
   UserIcon,
 } from "@heroicons/react/outline";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Header() {
+  const router = useRouter()
   return (
     <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
       <div className="flex flex-grow justify-evenly max-w-2xl">
-        <HeaderItem title="HOME" Icon={HomeIcon} />
-        <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
-        <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} />
-        <HeaderItem title="COLLECTIONS" Icon={CollectionIcon} />
-        <HeaderItem title="SEARCH" Icon={SearchIcon} />
-        <HeaderItem title="ACCOUNT" Icon={UserIcon} />
+        <Link href="/"><a><HeaderItem title="HOME" Icon={HomeIcon} /></a></Link>
+        <Link href="/?genre=fetchTrending"><a><HeaderItem title="TRENDING" Icon={LightningBoltIcon} /></a></Link>
+        <Link href="/"><a><HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} /></a></Link>
+        <Link href="/"><a><HeaderItem title="COLLECTIONS" Icon={CollectionIcon} /></a></Link>
+        <Link href="/"><a><HeaderItem title="SEARCH" Icon={SearchIcon} /></a></Link>
+        <Link href="/"><a><HeaderItem title="ACCOUNT" Icon={UserIcon} /></a></Link>
       </div>
       <Link href="/">
         <a>
