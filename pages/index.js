d61c9c7410c20b8ef6e500f1c05178b92ad56c83
vat.sal <0505m2003@gmail.com>
2022-05-14 10:47:06 +0530

    Debug4

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import Head from "next/head";
 import requests from "../utils/requests";
-import Header from "./components/Header";
-import Nav from "./components/Nav";
-import Results from "./components/Results";
+import Header from "../components/Header";
+import Nav from "../components/Nav";
+import Results from "../components/Results";
 
 export default function Home({ results }) {
   return (
